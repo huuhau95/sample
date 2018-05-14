f4675bd03f44eb94b1434c48344a0cacaba002c3
tungchem1607 <tungchem1607@gmail.com>
2018-05-14 18:18:13 +0000

    folow user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update, :show]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
+                                        :following, :followers]
   before_action :correct_user, only: [:edit, :update, :show]
 
   def index
@@ -50,7 +51,21 @@ class UsersController < ApplicationController
       flash[:success] = t:user_deleted
       redirect_to users_url
   end
+  
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.paginate(page: params[:page])
+    render 'show_follow'
+  end
 
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.paginate(page: params[:page])
+    render 'show_follow'
+  end
+  
   private 
     def user_params
       params.require(:user).permit :email, :name, :password, :password_confirmation
